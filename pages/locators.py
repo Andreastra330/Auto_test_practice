0f5d90d0eff72b5fd77ada4dd46fbc982f29d3b0
Andreastra330 <andreastra330@gmail.com>
2024-10-28 23:14:13 +0300

    update

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,8 @@ class LoginLocators:
     PASSWORD = (By.CSS_SELECTOR, "input[id=password-field-Password]")
     BUTTON_ENTER = (By.XPATH, "//button[text()='Войти']")
     BUTTON_RAZDEL = (By.CSS_SELECTOR, '[data-testid="section"]')
+    BUTTON_NEED_RAZDEL = (By.XPATH, "//*[text() = 'Конфигурирование']")
+    BUTTON_GROUP_REESTR = (By.XPATH, "//*[text() = 'Администрирование']")
+    BUTTON_REEST = (By.XPATH, "//*[text() = 'Шаблоны документов']")
+
+
